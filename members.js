6c07a76a6c5751564a9182fa2e3d3d8cc6e8e711
victorlazarus32 <victorlazarus32@gmail.com>
2026-01-01 13:16:42 -0500

    Update members.js

--- members.js
+++ members.js
@@ -1,10 +1,8 @@
-// Master list of members for autocomplete
 var DINK_MEMBERS = [
   "Test Player One",
   "Test Player Two",
   "Test Player Three"
 ];
 
-// Also expose on window, in case we ever need window.DINK_MEMBERS
 window.DINK_MEMBERS = DINK_MEMBERS;
 
